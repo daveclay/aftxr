3bb8fe32f5a2cc268f72ecb46bfc4e1947502b67
Dave Clay <daveclay@gmail.com>
2014-12-07 19:56:37 -0800

    moving bits

---
+++ src/main/webapp/js/aftxr/css.js
@@ -0,0 +1,47 @@
+(function() {
+    var vendor = (function () {
+        var vendors = 't,webkitT,MozT,msT,OT'.split(','),
+            dummyStyle = document.createElement('div').style,
+            t,
+            i = 0,
+            l = vendors.length;
+
+        for ( ; i < l; i++ ) {
+            t = vendors[i] + 'ransform';
+            if ( t in dummyStyle ) {
+                return vendors[i].substr(0, vendors[i].length - 1);
+            }
+        }
+
+        return false;
+    })();
+
+    function prefixStyle(style) {
+        if ( vendor === '' ) return style;
+
+        style = style.charAt(0).toUpperCase() + style.substr(1);
+        return vendor + style;
+    }
+
+    var CSSUtils = {
+        cssVendor: vendor ? '-' + vendor.toLowerCase() + '-' : '',
+        transformOrigin: prefixStyle('transformOrigin'),
+        transform: prefixStyle('transform'),
+        transitionDuration: prefixStyle('transitionDuration'),
+
+        transitionEndEvent: (function () {
+            if ( vendor === false ) return false;
+            var transitionEnd = {
+                ''			: 'transitionend',
+                'webkit'	: 'webkitTransitionEnd',
+                'Moz'		: 'transitionend',
+                'O'			: 'oTransitionEnd',
+                'ms'		: 'MSTransitionEnd'
+            };
+
+            return transitionEnd[vendor];
+        })()
+    };
+
+    window.CSSUtils = CSSUtils;
+})();
