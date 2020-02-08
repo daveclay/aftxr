bba3e959424ef792d018207f9dde44fff6484c9a
Dave Clay <dave@carezone.com>
2020-02-08 07:59:21 -0800

    Move webapp to .

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mvn clean package
-scp -i ~/.ssh/ec2-bitnami-daveclay.pem target/aftxr*.war aftxr.com:aftxr.war && ssh -i ~/.ssh/ec2-bitnami-daveclay.pem aftxr.com "./deploy-aftxr.sh"
