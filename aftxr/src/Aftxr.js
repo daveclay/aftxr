caef2cffdc063184bcec8536a10be81e1ace6a3b
Dave Clay <david.clay@walmart.com>
2021-01-11 17:50:57 -0800

    More ui tweaksing

--- aftxr/src/Aftxr.js
+++ aftxr/src/Aftxr.js
@@ -15,18 +15,18 @@ class Aftxr extends Component {
                     <div className="logo">AFTXR</div>
                     <div className="playerContainer">
                         <iframe title="The Faithful by aftxr"
-                                src="https://bandcamp.com/EmbeddedPlayer/album=2960334884/size=large/bgcol=333333/linkcol=e99708/minimal=true/transparent=true/"
+                                src="https://bandcamp.com/EmbeddedPlayer/album=2960334884/size=large/tracklist=false/artwork=small/bgcol=333333/linkcol=e99708/transparent=true/"
                                 seamless><a href="https://aftxr.bandcamp.com/album/the-faithful">The Faithful by aftxr</a></iframe>
                         <iframe title="Within Tolerances by aftxr"
-                                src="https://bandcamp.com/EmbeddedPlayer/album=2507150089/size=large/bgcol=333333/linkcol=e99708/minimal=true/transparent=true/"
+                                src="https://bandcamp.com/EmbeddedPlayer/album=2507150089/size=large/tracklist=false/artwork=small/bgcol=333333/linkcol=e99708/transparent=true/"
                                 seamless><a href="https://aftxr.bandcamp.com/album/within-tolerances">Within Tolerances by aftxr</a></iframe>
-                        <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2105057392/size=large/bgcol=333333/linkcol=e99708/minimal=true/transparent=true/"
+                        <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2105057392/size=large/tracklist=false/artwork=small/bgcol=333333/linkcol=e99708/transparent=true/"
                                 seamless><a href="https://aftxr.bandcamp.com/album/occido">Occido by aftxr</a></iframe>
-                        <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2087624922/size=large/bgcol=333333/linkcol=e99708/minimal=true/transparent=true/"
+                        <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2087624922/size=large/tracklist=false/artwork=small/bgcol=333333/linkcol=e99708/transparent=true/"
                                 seamless><a href="https://aftxr.bandcamp.com/album/absisto">Absisto by aftxr</a></iframe>
-                        <iframe src="https://bandcamp.com/EmbeddedPlayer/album=4136964018/size=large/bgcol=333333/linkcol=e99708/minimal=true/transparent=true/"
+                        <iframe src="https://bandcamp.com/EmbeddedPlayer/album=4136964018/size=large/tracklist=false/artwork=small/bgcol=333333/linkcol=e99708/transparent=true/"
                                 seamless><a href="https://aftxr.bandcamp.com/album/adrift">Adrift by aftxr</a></iframe>
-                        <iframe src="https://bandcamp.com/EmbeddedPlayer/track=1977380926/size=large/bgcol=333333/linkcol=e99708/minimal=true/transparent=true/"
+                        <iframe src="https://bandcamp.com/EmbeddedPlayer/track=1977380926/size=large/tracklist=false/artwork=small/bgcol=333333/linkcol=e99708/transparent=true/"
                                 seamless><a href="https://aftxr.bandcamp.com/track/vote">Vote by aftxr</a></iframe>
                     </div>
                 </div>
